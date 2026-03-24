9eb3baa37b19923ce0611f898e4280582c2a0bc9
metalbabble <brian@metalbabble.com>
2026-03-24 16:47:57 -0400

    ver

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Gameplay configuration
 export const config = {
-  version: '0.1.1',
+  version: '0.1.2',
 
   // Number of chunks rendered in each direction from the player.
   // Higher values show more of the world but cost more CPU/GPU. (Default: 5)
