45245532a47f997ba219f9fe29916f22383f1131
metalbabble <brian@metalbabble.com>
2026-03-25 19:21:40 -0400

    media qry to fix touch ui

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Gameplay configuration
 export const config = {
-  version: '0.2.5',
+  version: '0.2.6',
 
   // Message of the day shown on the main menu overlay. Set to '' to hide.
   motd: '** Latest update: added inventory system! **',
